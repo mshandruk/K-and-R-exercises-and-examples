e1d95a6aa27faa27f0b8d2aefc0273f0965a8cea
Maxim Shandruk <139689251+mshandruk@users.noreply.github.com>
2025-04-07 20:55:45 +0300

    Fix: aligned code with proper tabs

--- 05.06-pointer_arrays-pointers_to_pointers/e-5.7-modified_readlines.c
+++ 05.06-pointer_arrays-pointers_to_pointers/e-5.7-modified_readlines.c
@@ -21,7 +21,7 @@ int readlines(char *lineptr[], char *linestor, int maxlines)
 	line[len-1] = '\0';   // delete newline
 	strcpy(p, line);
 	lineptr[nlines++] = p;
-    p += len;
+	p += len;
       }
   return nlines;
 }
